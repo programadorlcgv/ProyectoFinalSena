f5a5a1b282d47516b36727fd966a139ba9f7b421
programadorlcgv <programadorlcgv@gmail.com>
2024-03-04 17:21:23 -0500

    Se Añade la opción de adjuntar imágenes al producto creado con funcionalidad completa.

--- tienda-online-admin/components/ProductForm.js
+++ tienda-online-admin/components/ProductForm.js
@@ -1,23 +1,27 @@
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import Spinner from "./Spinner";
+import { ReactSortable } from "react-sortablejs";
 
 
 export default function ProductForm({
   _id,
   title:existingTitle, 
   description:existingDescription, 
-  price:existingPrice
+  price:existingPrice,
+  images:existingImages,
   }) {
   const [title, setTitle] = useState(existingTitle || '');
   const [description, setDescription] = useState(existingDescription || '');
   const [price, setPrice] = useState(existingPrice || '');
+  const [images, setImages] = useState(existingImages || []);
   const [goToProducts, setGoToProducts] = useState(false);
+  const [isUploading, setIsUploading] = useState(false);
   const router = useRouter();
-  console.log({_id})
   async function saveProduct(ev) {
     ev.preventDefault();
-    const data = {title, description, price};
+    const data = {title, description, price, images};
     if (_id) {
       //update 
       await axios.put('/api/products', {...data,_id});
@@ -30,6 +34,24 @@ export default function ProductForm({
   if (goToProducts) {
     router.push('/products');
   }
+  async function uploadImages(ev) {
+   const files = ev.target?.files;
+   if (files?.length > 0) {
+      setIsUploading(true);
+      const data = new FormData();
+      for (const file of files) {
+        data.append('file', file);
+      }
+      const res = await axios.post('/api/upload', data);
+      setImages(oldImages => {
+        return [...oldImages, ...res.data.links];
+      });
+      setIsUploading(false);
+   }
+  }
+  function uploadImagesOrder(images) {
+    setImages(images);
+  }
   return (
     <form onSubmit={saveProduct}>
     <label>Nombre</label>
@@ -38,6 +60,35 @@ export default function ProductForm({
       placeholder="Nombre del producto" 
       value={title}
       onChange={ev => setTitle(ev.target.value)} />
+    <label>
+      Fotos
+    </label>
+    <div className="mb-2 flex flex-wrap gap-1">
+      <ReactSortable 
+      list={images} 
+      className="flex flex-wrap gap-1"
+      setList={uploadImagesOrder}>
+      {!!images?.length && images.map(link =>(
+        <div key={link} className="h-24">
+          <img src={link} alt="" className="rounded-lg"/>
+        </div>
+      ))}
+      </ReactSortable>
+      {isUploading && (
+        <div className="h-28 flex items-center">
+          <Spinner />
+        </div>
+      )}
+      <label className="w-28 h-28 text-center flex flex-col items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200 cursor-pointer">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+        </svg>
+        <div>
+          Subir imagen
+        </div>
+        <input type="file" onChange={uploadImages} className="hidden"/>
+      </label>
+    </div>
     <label>Descripción</label>
     <textarea 
       placeholder="Descripción de Producto"
--- tienda-online-admin/models/Product.js
+++ tienda-online-admin/models/Product.js
@@ -4,6 +4,7 @@ const ProductSchema = new Schema({
     title: {type:String, require:true},
     description:{type:String, require:true},
     price: {type:Number, require:true},
+    images: [{type:String}],
 });
 
 export const Product = models.Product ||  model('Product', ProductSchema);
