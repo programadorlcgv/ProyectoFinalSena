e2645faa74ddb0654b4dc38f1a7eedf66078b346
programadorlcgv <programadorlcgv@gmail.com>
2024-03-05 12:50:37 -0500

    Se incorpora la capacidad de editar las categorías existentes.

--- tienda-online-admin/pages/categories.js
+++ tienda-online-admin/pages/categories.js
@@ -3,22 +3,44 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Categories() {
+  const [editedCategory, setEditedCategory] = useState(null);
   const [name, setName] = useState('');
+  const [parentCategory, setParentCategory] = useState('');
   const [categories, setCategories] = useState([]);
   useEffect(() => {
+   fechtCategories();
+  }, []);
+  function fechtCategories() {
     axios.get('/api/categories').then(result => {
       setCategories(result.data);
     });
-  }, []);
+  }
   async function saveCategory(ev) {
     ev.preventDefault();
-    await axios.post('/api/categories', {name});
+    const data = {name,parentCategory}
+    if (editedCategory) {
+      data._id = editedCategory._id;
+      await axios.put('/api/categories', data);
+      setEditedCategory(null);
+    } else {
+      await axios.post('/api/categories', data);
+    }
     setName('');
+    fechtCategories();
+  }
+  function editCategory(category) {
+    setEditedCategory(category);
+    setName(category.name);
+    setParentCategory(category.parent?._id);
   }
   return (
     <Layout>
       <h1>Categorias</h1>
-      <label>Nueva categoria</label>
+      <label>
+        {editedCategory 
+        ? `Editar Categoria ${editedCategory.name}`
+        : 'Crear nueva categoria'}
+      </label>
       <form onSubmit={saveCategory} className="flex gap-1">
       <input 
       className="mb-0" 
@@ -26,20 +48,42 @@ export default function Categories() {
       placeholder={'Nombre de categoria'} 
       onChange={ev => setName(ev.target.value)}
       value={name}/>
+      <select className="mb-0" 
+        onChange={ev => setParentCategory(ev.target.value)}
+        value={parentCategory}>
+        <option value="">Sin categoria principal</option>
+        {categories.length > 0 && categories.map(category => (
+            <option value={category._id}>{category.name}</option>
+          ))}
+      </select>
       <button type="submit" className="btn-primary py-1">Guardar</button>
       </form>
       <table className="basic mt-4">
         <thead>
           <tr>
-            <td>
-              Nombre de Categoria
-            </td>
+            <td>Nombre de Categoria</td>
+            <td>Categoria padre</td>
+            <td></td>
           </tr>
         </thead>
         <tbody>
           {categories.length > 0 && categories.map(category => (
             <tr>
               <td>{category.name}</td>
+              <td>{category?.parent?.name}</td>
+              <td>
+                <button 
+                  onClick={() => editCategory(category)}
+                  className="btn-primary mr-1"
+                >
+                  Editar
+                </button>
+                <button 
+                  className="btn-primary"
+                >
+                  Borrar
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
