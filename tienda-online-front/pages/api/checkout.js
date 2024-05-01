3421a319d3833a2a3bc68ad6ea2fc4dbd5ec6e7f
programadorlcgv <programadorlcgv@gmail.com>
2024-05-01 10:12:38 -0500

    Se ha establecido la conexión a la base de datos para permitir la visualización de las compras registradas por los usuarios en el frontend desde el panel de administración.

--- tienda-online-front/pages/api/checkout.js
+++ tienda-online-front/pages/api/checkout.js
@@ -29,7 +29,7 @@ export default async function handler(req,res) {
         price_data: {
           currency: 'COP',
           product_data: {name: productInfo.title},
-          unit_amount: quantity * productInfo.price ,  //precio que se muestra de la plataforma de pago
+          unit_amount: quantity * productInfo.price * 100 ,  //precio que se muestra de la plataforma de pago
         }
       });
     }
--- tienda-online-front/pages/cart.js
+++ tienda-online-front/pages/cart.js
@@ -53,7 +53,7 @@ const CityHolder = styled.div`
 
 
 export default function CartPage() {
-  const {cartProducts, addProduct,removeProduct} = useContext(CartContext);
+  const {cartProducts, addProduct,removeProduct,clearCart} = useContext(CartContext);
   const [products, setProducts] = useState([]);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -61,6 +61,7 @@ export default function CartPage() {
   const [postalCode, setPostalCode] = useState('');
   const [streetAddress, setStreetAddress] = useState('');
   const [country, setCountry] = useState('');
+  const [isSuccess,setIsSuccess] = useState(false);
   useEffect(() => {
     if (cartProducts.length > 0) {
       axios.post('/api/cart', {ids:cartProducts})
@@ -71,6 +72,15 @@ export default function CartPage() {
       setProducts([]);
     }
   }, [cartProducts]);
+  useEffect(() => {
+    if (typeof window === 'undefined') {
+      return;
+    }
+    if (window?.location.href.includes('success')) {
+      setIsSuccess(true);
+      clearCart();
+    }
+  }, []);
   function moreOfThisProduct(id) {
     addProduct(id);
   }
@@ -92,9 +102,7 @@ export default function CartPage() {
     total += price;
   }
   
-
-  // se presenta el problema de handle error revisar 
-  if ( typeof window !== 'undefined' && window.location.href.includes('success')) {
+  if (isSuccess) {
     return (
       <>
         <Header />
