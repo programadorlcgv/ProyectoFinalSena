48c6e66f8585e4f4c60d4e77c9c3b1c53c7c59f1
programadorlcgv <programadorlcgv@gmail.com>
2024-10-26 10:33:12 -0500

    Se ha añadido la funcionalidad de favoritos y se ha validado que, en la sección de cuenta de usuario, no se generen errores al mostrar los detalles de la cuenta y los favoritos antes de iniciar sesión. Esta mejora garantiza una experiencia de usuario más estable y sin interrupciones.

--- tienda-online-front/components/ProductBox.js
+++ tienda-online-front/components/ProductBox.js
@@ -1,11 +1,14 @@
-import styled from "styled-components"
-import Button, { ButtonStyle } from "./Button";
-import CartIcon from "./icons/CartIcon";
+import styled from "styled-components";
+import Button, {ButtonStyle} from "@/components/Button";
+import CartIcon from "@/components/icons/CartIcon";
 import Link from "next/link";
-import { useContext } from "react";
-import { CartContext } from "./CartContext";
-import FlyingButton from "./FlyingButton";
-
+import {useContext, useEffect, useState} from "react";
+import {CartContext} from "@/components/CartContext";
+import {primary} from "@/lib/colors";
+import FlyingButton from "@/components/FlyingButton";
+import HeartOutlineIcon from "@/components/icons/HeartOutlineIcon";
+import HeartSolidIcon from "@/components/icons/HeartSolidIcon";
+import axios from "axios";
 
 const ProductWrapper = styled.div`
   button{
@@ -14,31 +17,33 @@ const ProductWrapper = styled.div`
     justify-content: center;
   }
 `;
+
 const WhiteBox = styled(Link)`
- background-color: #fff;
- padding: 20px;
- height: 120px;
- text-align: center;
- display: flex;
- align-items: center;
- justify-content: center;
- border-radius: 10px;
- img{
-  max-width: 100%;
-  max-height: 150px;
- }
+  background-color: #fff;
+  padding: 20px;
+  height: 150px;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  position: relative;
+  img{
+    max-width: 100%;
+    max-height: 130px;
+  }
 `;
 
 const Title = styled(Link)`
- font-weight: normal;
- font-size: .9rem;
- color: inherit;
- text-decoration:none;
- margin: 0;
+  font-weight: normal;
+  font-size:.9rem;
+  color:inherit;
+  text-decoration:none;
+  margin:0;
 `;
 
-const ProductInfoBox = styled.div `
- margin-top: 5px;
+const ProductInfoBox = styled.div`
+  margin-top: 5px;
 `;
 
 const PriceRow = styled.div`
@@ -54,7 +59,7 @@ const PriceRow = styled.div`
 
 const Price = styled.div`
   font-size: 1rem;
-  font-weight:600;
+  font-weight:400;
   text-align: right;
   @media screen and (min-width: 768px) {
     font-size: 1.2rem;
@@ -63,25 +68,63 @@ const Price = styled.div`
   }
 `;
 
+const WishlistButton = styled.button`
+  border:0;
+  width: 40px !important;
+  height: 40px;
+  padding: 10px;
+  position: absolute;
+  top:0;
+  right:0;
+  background:transparent;
+  cursor: pointer;
+  ${props => props.wished ? `
+    color:red;
+  ` : `
+    color:black;
+  `}
+  svg{
+    width: 16px;
+  }
+`;
 
-export default function ProductBox ({_id,title,description,price,images}) {
-  
+export default function ProductBox({
+  _id,title,description,price,images,wished=false,
+  onRemoveFromWishlist=()=>{},
+}) {
   const url = '/product/'+_id;
+  const [isWished,setIsWished] = useState(wished);
+  function addToWishlist(ev) {
+    ev.preventDefault();
+    ev.stopPropagation();
+    const nextValue = !isWished;
+    if (nextValue === false && onRemoveFromWishlist) {
+      onRemoveFromWishlist(_id);
+    }
+    axios.post('/api/wishlist', {
+      product: _id,
+    }).then(() => {});
+    setIsWished(nextValue);
+  }
   return (
     <ProductWrapper>
       <WhiteBox href={url}>
         <div>
+          <WishlistButton wished={isWished} onClick={addToWishlist}>
+            {isWished ? <HeartSolidIcon /> : <HeartOutlineIcon />}
+          </WishlistButton>
           <img src={images?.[0]} alt=""/>
         </div>
-    </WhiteBox>
-    <ProductInfoBox>
-      <Title href={url}>{title}</Title>
-      <PriceRow>
-        <Price>${price}</Price>
-        <FlyingButton _id={_id} src={images?.[0]}>Añadir al carrito</FlyingButton>
-      </PriceRow> 
-    </ProductInfoBox>
+      </WhiteBox>
+      <ProductInfoBox>
+        <Title href={url}>{title}</Title>
+        <PriceRow>
+          <Price>
+            ${price}
+          </Price>
+          <FlyingButton _id={_id} src={images?.[0]}>Añadir al carrito</FlyingButton>
+        </PriceRow>
+      </ProductInfoBox>
     </ProductWrapper>
-    
-  )
+  );
 }
