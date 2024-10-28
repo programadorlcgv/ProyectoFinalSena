a65fc15d67e647fb23df6680595a1452066a0e59
programadorlcgv <programadorlcgv@gmail.com>
2024-10-28 10:08:48 -0500

    Se ha agregado la funcionalidad que permite a los usuarios ver el historial de sus compras realizadas.

--- tienda-online-front/pages/api/checkout.js
+++ tienda-online-front/pages/api/checkout.js
@@ -1,6 +1,8 @@
 import { mongooseConnect } from "@/lib/mongoose";
 import { Order } from "@/models/Order";
 import { Product } from "@/models/Product";
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/pages/api/auth/[...nextauth]";
 
 const stripe = require('stripe')(process.env.STRIPE_SK);
 
@@ -35,12 +37,15 @@ export default async function handler(req,res) {
     }
   }
 
+  const session = await getServerSession(req,res,authOptions);
+
   const orderDoc = await Order.create({
     line_items,name,email,city,postalCode,
     streetAddress,country,paid:false,
+    userEmail:session?.user?.email,
   });
 
-  const session = await stripe.checkout.sessions.create({
+  const stripeSession = await stripe.checkout.sessions.create({
     line_items,
     mode: 'payment',
     customer_email: email,
@@ -50,7 +55,7 @@ export default async function handler(req,res) {
     })
 
     res.json({
-      url:session.url,
+      url:stripeSession.url,
     })
 }
 
