243652c65f03ce280f4d19b249137d9927b0e734
programadorlcgv <programadorlcgv@gmail.com>
2024-11-07 10:58:21 -0500

    Se añadió una sección de configuraciones para administradores, donde ahora pueden modificar el producto destacado que se muestra a los usuarios de la tienda. Además, se incorporó la opción de establecer el precio del envío, el cual se refleja tanto en el carrito de compras como en el proceso de pago con Stripe.

--- tienda-online-front/pages/index.js
+++ tienda-online-front/pages/index.js
@@ -1,34 +1,35 @@
-import Featured from "@/components/Featured";
 import Header from "@/components/Header";
+import Featured from "@/components/Featured";
+import {Product} from "@/models/Product";
+import {mongooseConnect} from "@/lib/mongoose";
 import NewProducts from "@/components/NewProducts";
-import { mongooseConnect } from "@/lib/mongoose";
-import { Product } from "@/models/Product";
-import { WishedProduct } from "@/models/WishedProduct";
+import {WishedProduct} from "@/models/WishedProduct";
 import {getServerSession} from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]";
-
+import {authOptions} from "@/pages/api/auth/[...nextauth]";
+import {Setting} from "@/models/Setting";
 
 export default function HomePage({featuredProduct,newProducts,wishedNewProducts}) {
   return (
-   <div>
-    <Header />
-    <Featured product ={featuredProduct}/>
-    <NewProducts products={newProducts} wishedProducts={wishedNewProducts} />
-   </div>
+    <div>
+      <Header />
+      <Featured product={featuredProduct} />
+      <NewProducts products={newProducts} wishedProducts={wishedNewProducts} />
+    </div>
   );
 }
 
 export async function getServerSideProps(ctx) {
-  const featuredProductId = '65e1fa373ed66cd0aa322369';
   await mongooseConnect();
+  const featuredProductSetting = await Setting.findOne({name:'featuredProductId'});
+  const featuredProductId = featuredProductSetting.value;
   const featuredProduct = await Product.findById(featuredProductId);
   const newProducts = await Product.find({}, null, {sort: {'_id':-1}, limit:10});
   const session = await getServerSession(ctx.req, ctx.res, authOptions);
   const wishedNewProducts = session?.user
     ? await WishedProduct.find({
         userEmail:session.user.email,
-        product: newProducts.map (p => p._id.toString()),
-      }) 
+        product: newProducts.map(p => p._id.toString()),
+      })
     : [];
   return {
     props: {
@@ -38,4 +39,3 @@ export async function getServerSideProps(ctx) {
     },
   };
 }
-
